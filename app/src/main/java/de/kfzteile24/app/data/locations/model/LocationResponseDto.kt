14a953de591fb631bfb8a0e9363496c2305f3857
Othman <othman.eljazouli@kfzteile24.de>
2022-02-07 06:08:15 +0100

    no message

---
+++ app/src/main/java/de/kfzteile24/app/data/locations/model/LocationResponseDto.kt
@@ -0,0 +1,18 @@
+package de.kfzteile24.app.data.locations.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LocationResponseDto(
+  @SerialName("data")
+  val locationData: List<LocationData>
+)
+
+@Serializable
+data class LocationData(
+  @SerialName("id")
+  val id: Int,
+  @SerialName("name")
+  val name: String
+)
